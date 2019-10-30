cd4034e6aee5d7349ba6102c7181f4c397ad7d5b
Josh Glaser <joshua.glaser@pcc.edu>
2019-10-29 20:52:51 -0700

    added main(), also changed capitalization in name :)

---
+++ src/cis/hello/world/helloWorld.java
@@ -0,0 +1,7 @@
+package cis.hello.world;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
--- src/cis/hello/world/helloworld.java
+++
@@ -1,4 +0,0 @@
-package cis.hello.world;
-
-public class helloworld {
-}
