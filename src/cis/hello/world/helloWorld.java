06eb3bf4380d0d5ecce25708000883760875f581
Josh Glaser <joshua.glaser@pcc.edu>
2019-10-29 20:57:17 -0700

    added date, also added a header which I forgot to do earlier

--- src/cis/hello/world/helloWorld.java
+++ src/cis/hello/world/helloWorld.java
@@ -1,7 +1,14 @@
 package cis.hello.world;
 
+import java.util.Date;
+
+// A simple hello world program
+// @Author: Josh Glaser
+// @Version: 2019.10.29
+
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println(new Date());
     }
 }
